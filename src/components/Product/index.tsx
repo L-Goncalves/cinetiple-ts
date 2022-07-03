b175317fa46c35f4b10fcfd3e4a567fbe312a713
Lucas Gonçalves <ldgoncalvs@gmail.com>
2022-07-03 19:35:22 -0300

    refactoring part of the product page

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -2,9 +2,6 @@ import "./index.scss";
 
 export const ProductDetailsAndPayment = () => {
   return (
-   
-
-    
     <div className="product_details">
       <img
         className="product_details_image"
@@ -13,46 +10,100 @@ export const ProductDetailsAndPayment = () => {
       />
 
       <div className="product_details_container">
-      <div className="product_details_info">
-        <p className="product_details_info_title">ICE</p>
-        <p>Produtor: CInetiple</p>
-        <br />
-        <p>Descrição</p>
-        <p>
-          Ultrices fames tristique quis ipsum maecenas nam ipsum mi sit.
-          Commodo, praesent quam volutpat lectus mauris senectus etiam lorem eu.
-          Nec sollicitudin tincidunt tincidunt quis nibh sit. Semper in ac
-          mollis posuere nulla. In vel, nam justo arcu cursus...Ler mais.
-        </p>
-        <div>
-        <h1>Tags</h1>
-        <Tags/>
+        <div className="product_details_info">
+          <ProductDetails />
+          <Tags />
+          <Details/>
+        </div>
+        <div className="product_details_pricing">
+          <PricingDetails />
+          <AdditionalDetails/>
         </div>
       </div>
-
-      <div className="product_details_pricing">
-        <p className="product_details_pricing_title">Á VISTA POR:</p>
-        <h1 className="product_details_pricing_price">R$ 39,90</h1>
-      
-        <p>EM 6X R$ 6,65 SEM JUROS</p>
-        <p>MÉTODOS DE PAGAMENTO</p>
-        <button>Comprar</button>
-        <p>Adicionar ao Carrinho</p>
-      </div>
-      </div>
-   
-     
     </div>
-
   );
 };
 
 export const Tags = () => {
-   return (<div className="container">
- 
-    {['Cymbals','Kicks','Snares','Cymbals','Kicks','Snares','Cymbals','Kicks','Snares'].map(item => {
-      return <div className="product_details_tags_card">{item}</div>
-    })}
+  return (
+    <>
+      <h2>Tags</h2>
 
- </div>)
+      <div className="container">
+        {[
+          "Cymbals",
+          "Kicks",
+          "Snares",
+          "Cymbals",
+          "Kicks",
+          "Snares",
+          "Cymbals",
+          "Kicks",
+          "Snares",
+        ].map((item) => {
+          return <div className="product_details_tags_card">{item}</div>;
+        })}
+      </div>
+    </>
+  );
 };
+
+const ProductDetails = () => {
+  return (
+    <>
+      <p className="product_details_info_title">ICE</p>
+      <p>Produtor: CInetiple</p>
+      <br />
+      <p>Descrição</p>
+      <p>
+        Ultrices fames tristique quis ipsum maecenas nam ipsum mi sit. Commodo,
+        praesent quam volutpat lectus mauris senectus etiam lorem eu. Nec
+        sollicitudin tincidunt tincidunt quis nibh sit. Semper in ac mollis
+        posuere nulla. In vel, nam justo arcu cursus...Ler mais.
+      </p>
+    </>
+  );
+};
+
+const PricingDetails = () => {
+  return (
+    <>
+      <p className="product_details_pricing_title">Á VISTA POR:</p>
+      <h1 className="product_details_pricing_price">R$ 39,90</h1>
+
+      <p>EM 6X R$ 6,65 SEM JUROS</p>
+      <p>MÉTODOS DE PAGAMENTO</p>
+      <button>Comprar</button>
+      <p>Adicionar ao Carrinho</p>
+    </>
+  );
+};
+
+
+const Details = () => {
+  return <div className="details">
+    
+    
+    <p className="details_title"> Caracteristicas</p>
+    <p className="details_subtitle">Tamanho</p>
+    <p>124MB</p>
+
+    <p className="details_subtitle">Número de Samples</p>
+    <p>1.256</p>
+    <p className="details_subtitle">Formatos de Arquivo</p>
+    <p>.wav/.mp3/.midi</p>
+  </div>
+}
+
+const AdditionalDetails = () => {
+  return <div className="details_rightside">
+    
+  <p className="details_subtitle">Categoria</p>
+  <p>Sample Pack</p>
+
+  <p className="details_subtitle">Gênero</p>
+  <p>Ambience</p>
+  <p className="details_subtitle">Licença</p>
+  <p>Sem direitos autorais</p>
+</div>
+}
