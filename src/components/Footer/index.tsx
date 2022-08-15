698e150a11f27648a4d51c70a80fde60e1f59efc
Lucas Gonçalves <ldgoncalvs@gmail.com>
2022-08-15 01:31:57 -0300

    add: no referrer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -11,10 +11,10 @@ export const Footer = () => {
           src={process.env.PUBLIC_URL + "/Logo.svg"}
         />
         <div className="links">
-          <a href="https://www.twitter.com">Terms of Service</a>
-          <a href="https://www.twitter.com">Privacy Policy</a>
-          <a href="https://www.twitter.com">Contact Us</a>
-          <a href="https://www.twitter.com">About</a>
+          <a rel="noreferrer" href="https://www.twitter.com">Terms of Service</a>
+          <a rel="noreferrer" href="https://www.twitter.com">Privacy Policy</a>
+          <a rel="noreferrer" href="https://www.twitter.com">Contact Us</a>
+          <a rel="noreferrer" href="https://www.twitter.com">About</a>
         </div>
 
         <div className="copyright">
@@ -24,19 +24,19 @@ export const Footer = () => {
       <div className="social">
         <p>Nos Siga!</p>
 
-        <a target="_blank" href="https://www.twitter.com">
+        <a target="_blank" rel="noreferrer" href="https://www.twitter.com">
         <Twitter width={40} />
         </a>
 
-        <a target="_blank" href="https://www.youtube.com">
+        <a target="_blank" rel="noreferrer" href="https://www.youtube.com">
         <Youtube width={40} />
         </a>
 
-        <a target="_blank" href="https://www.instagram.com/cinetiple">
+        <a target="_blank" rel="noreferrer" href="https://www.instagram.com/cinetiple">
         <Instagram width={40} />
         </a>
         
-        <a  target="_blank" href="https://www.facebook.com"> 
+        <a  target="_blank" rel="noreferrer" href="https://www.facebook.com"> 
         <Facebook width={40} />
         </a>
        
