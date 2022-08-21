54e2f8651a40f5d1669ac3d58df5f97fec325206
Lucas Gonçalves <ldgoncalvs@gmail.com>
2022-08-21 17:44:10 -0300

    feat: adding animation to modal

--- src/components/LoginCreateAccount/index.tsx
+++ src/components/LoginCreateAccount/index.tsx
@@ -5,38 +5,40 @@ import './index.scss'
 
 function Modal({open, onClose}) {
     return createPortal(
-      <div className={open ? 'modal-wrapper' : ''}> 
+      <div className={open ? 'modal-wrapper' : 'modal-wrapper-closed'}> 
         <div className={open ? 'modal': 'modal-closed'}> 
-        <div className='modal_login_container'>
+        {open  ? (<>
+          <div className='modal_login_container'>
         
-          <div className='title'>
-             <h1>Faça seu login</h1>
-          </div>
-          <div className='modal_login_body'>
-              <InputForm type={"email"}/>
-              <InputForm type={"password"}/>
-              <button className='modal_login_button'>Continuar</button>
-          </div>
-          <div className='modal_login_footer'>
-            <p>
-            Esqueceu a senha?
-            </p>
-          
-
-            <p>
-              Ainda não tem conta? Crie Agora!
-            </p>
-          
-          </div>
+        <div className='title'>
+           <h1>Faça seu login</h1>
         </div>
-      
-        <div className='modal_welcome'>
-        <button className='modal_welcome_close' onClick={onClose}>X</button>
-         <h1>Bem vindo à </h1>
-         <Logo width={270}  height={100}/>
+        <div className='modal_login_body'>
+            <InputForm type={"email"}/>
+            <InputForm type={"password"}/>
+            <button className='modal_login_button' onClick={onClose}>Continuar</button>
+        </div>
+        <div className='modal_login_footer'>
+          <p>
+          Esqueceu a senha?
+          </p>
+        
 
+          <p>
+            Ainda não tem conta? Crie Agora!
+          </p>
         
         </div>
+      </div>
+    
+      <div className='modal_welcome'>
+      <button className='modal_welcome_close' onClick={onClose}>X</button>
+       <h1>Bem vindo à </h1>
+       <Logo width={270}  height={100}/>
+
+
+      </div>
+        </>) :  (<></>) }
         
         </div>
         </div>,
